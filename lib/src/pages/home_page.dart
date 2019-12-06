534d57a58e1aca39e169f65af88e49dfa161213f
unknown <nunja.zeus@gmail.com>
2019-12-05 21:56:56 -0500

    column

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
+//TextStyle estiloTexto = new TextStyle( fontSize: 30);
+final estiloTexto = new TextStyle( fontSize: 30);
 
 @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Titulo'),
+        title: Text('Titulo_Cartelera'),
+        centerTitle: true ,
+        elevation: 0.1,
       ),
       body: Center(
-        child: Text('El body yikyik'),
+        child: Column(          
+          mainAxisAlignment:  MainAxisAlignment.center,
+          children: <Widget>[            
+            // Text('Numeros de clicks yikyik', style: TextStyle(fontSize: 25)),
+            // Text('0', style: TextStyle(fontSize: 25)),
+            Text('Numeros de clicks yikyik', style: estiloTexto),
+            Text('0', style: estiloTexto),
+
+          ],  
+        ),
       ),
     );
   }
